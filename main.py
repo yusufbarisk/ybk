1d65e7b1d8096a8664f420004d14916dc7250819
yusufbarisk <yusufbarisk2004@gmail.com>
2020-10-25 14:40:27 +0300

    first changes

---
+++ main.py
@@ -0,0 +1,22 @@
+def main(new_width=100):
+    # attempt to open image from user-input
+    path = input("Enter a valid pathname to an image:\n")
+    
+    image = PIL.Image.open(path)
+    
+    # convert image to ascii    
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+    
+    # format
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    
+    # print result
+    print(ascii_image)
+    
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+ 
+# run program
+main()
